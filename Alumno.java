54788f038db8b8055499c6462f1f5c2ba56b2aba
oscar <oscarbarragan31@gmail.com>
2021-01-18 09:01:02 +0000

    3333.02 modifique el getnombreusuariogithub para que te cree un usuaio siempre

--- Alumno.java
+++ Alumno.java
@@ -1,5 +1,5 @@
 public class Alumno {
-    
+
     // el nombre completo del alumno
     private String nombre;
     // el numero de matricula
@@ -14,10 +14,10 @@ public class Alumno {
         nombre = nombreCompleto;
         numeroMatricula = numeroMatriculaAlumno;
         edad = edadAlumno;
-         if(nombre.length() < 3){
+        if(nombre.length() < 3){
             System.out.println("El nombre tiene menos de 3 caracteres");
         }
-        
+
         if(numeroMatricula.length() < 4){
             System.out.println("La matricula tiene menos de 4 caracteres");
         }
@@ -29,26 +29,40 @@ public class Alumno {
     public String getNombre() {
         return nombre;
     }
-    
+
     /**
      * Cambia el nombre del alumno
      */
     public void cambiarnombre(String nuevoNombre) {
         nombre = nuevoNombre;
     }
-    
+
     /**
      * Imprime por pantalla los detalles del alumno
      */
     public void imprimeDetalles() {
         System.out.println(nombre + " (" + numeroMatricula + ") - " + edad + " años");
     }
-    
+
     /**
      * Devuelve el nombre de usuario que el alumno debe configurar
      * en su cuenta de Github en formato de 7 caracateres
      */
     public String getNombreUsuarioGithub() {
-        return nombre.substring(0, 3) + numeroMatricula.substring(0, 4);
+        if (nombre.length() < 3){ 
+            nombre = nombre.substring(0, 2);
+        }
+        else if (nombre.length() >= 3){
+            nombre = nombre.substring(0, 3);
+        }
+        
+        if(numeroMatricula.length() < 4){
+            numeroMatricula = numeroMatricula.substring(0, 3);
+        }
+        else if(numeroMatricula.length() >= 4){
+            numeroMatricula = numeroMatricula.substring(0, 4);
+        }
+
+        return nombre + numeroMatricula;
     }
 }
